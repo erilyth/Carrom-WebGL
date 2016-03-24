c5620318194badb7e79bc94cd1ac9fe8ea0a2a39
erilyth <vishal.batchu@students.iiit.ac.in>
2016-03-24 19:20:22 +0530

    Fix board to hole ratio

--- webgl.js
+++ webgl.js
@@ -2,8 +2,10 @@ var canvas,gl,program;
 
 var models = {};
 var overlapMargin = 0.0002;
+var screenVisible = 0;
 
-var viewMatrix = makeScale(1, 1, 1);
+var cameraMatrix = makeScale(1, 1, 1);
+var MVPMatrix = makeScale(1, 1, 1);
 
 function initViewport(gl, canvas)
 {
@@ -93,38 +95,35 @@ function Initialize()
 	program = createProgramFromScripts(gl,"2d-vertex-shader", "2d-fragment-shader");
 	gl.useProgram(program);
 
-  //Set this viewMatrix before calling the makeModel function, this can be treated as the camera.
-  //Assuming individual objects won't have any rotation.
-  //viewMatrix = makePerspective(180 * (3.14/180), 1, 0, 5000);
-  viewMatrix = matrixMultiply(viewMatrix, makeXRotation(90 * (3.14/180)));
-  viewMatrix = matrixMultiply(viewMatrix, makeYRotation(60 * (3.14/180)));
-  viewMatrix = matrixMultiply(viewMatrix, makeXRotation(-40 * (3.14/180)));
-  viewMatrix = matrixMultiply(viewMatrix, makeZToWMatrix(0.4));
+  //Set this MVPMatrix before calling the makeModel function, this can be treated as the camera.
+  //**Assuming individual objects won't have any rotation!! To do this store separate matrices for each object
+  // which would give us the individual scaling/rotation of each object. 
+  //MVPMatrix = makePerspective(180 * (3.14/180), 1, 0, 5000);
   makeModel('boardinner', 0, 0, 0, 1.5, 1.5, 0.03, 'boardinner.data');
   makeModel('boardouter1', 0, 0.75, 0, 1.6, 0.1, 0.15, 'boardouter.data');
   makeModel('boardouter2', 0, -0.75, 0, 1.6, 0.1, 0.15, 'boardouter.data');
   makeModel('boardouter3', -0.75, 0, 0, 0.1, 1.6, 0.15, 'boardouter.data');
   makeModel('boardouter4', 0.75, 0, 0, 0.1, 1.6, 0.15, 'boardouter.data');
-  makeModel('boardline', 0, 0, -overlapMargin, 1.05, 1.05, 0.03, 'boardouter.data');
-  makeModel('boardline2', 0, 0, -2*overlapMargin, 1.02, 1.02, 0.03, 'boardinner.data');
+  makeModel('boardline', 0, 0, -overlapMargin, 0.97, 0.97, 0.03, 'boardouter.data');
+  makeModel('boardline2', 0, 0, -2*overlapMargin, 0.95, 0.95, 0.03, 'boardinner.data');
   makeModel('cylindercenter1', 0, 0, 0, 0.2, 0.2, 0.018, 'cylinder.data');
   makeModel('cylindercenter2', 0, 0, 0, 0.185, 0.185, 0.018+overlapMargin, 'cylinderlight.data');
-  makeModel('cylinderside1', -0.468, -0.472, 0, 0.05, 0.05, 0.018, 'cylinder.data');
-  makeModel('cylinderside2', -0.468, -0.472, 0, 0.04, 0.04, 0.018+overlapMargin, 'cylinderlight.data');
-  makeModel('cylinderside3', 0.475, -0.465, 0, 0.05, 0.05, 0.018, 'cylinder.data');
-  makeModel('cylinderside4', 0.475, -0.465, 0, 0.04, 0.04, 0.018+overlapMargin, 'cylinderlight.data');
-  makeModel('cylinderside5', -0.472, 0.465, 0, 0.05, 0.05, 0.018, 'cylinder.data');
-  makeModel('cylinderside6', -0.472, 0.465, 0, 0.04, 0.04, 0.018+overlapMargin, 'cylinderlight.data');
-  makeModel('cylinderside7', 0.462, 0.472, 0, 0.05, 0.05, 0.018, 'cylinder.data');
-  makeModel('cylinderside8', 0.462, 0.472, 0, 0.04, 0.04, 0.018+overlapMargin, 'cylinderlight.data');
-  makeModel('goal1', -0.62, -0.65, 0, 0.08, 0.08, 0.018, 'cylinder.data');
-  makeModel('goal2', 0.65, -0.62, 0, 0.08, 0.08, 0.018, 'cylinder.data');
-  makeModel('goal3', -0.65, 0.62, 0, 0.08, 0.08, 0.018, 'cylinder.data');
-  makeModel('goal4', 0.62, 0.65, 0, 0.08, 0.08, 0.018, 'cylinder.data');
-  makeModel('goal1inner', -0.62, -0.65, 0, 0.065, 0.065, 0.018+overlapMargin, 'cylindergrey.data');
-  makeModel('goal2inner', 0.65, -0.62, 0, 0.065, 0.065, 0.018+overlapMargin, 'cylindergrey.data');
-  makeModel('goal3inner', -0.65, 0.62, 0, 0.065, 0.065, 0.018+overlapMargin, 'cylindergrey.data');
-  makeModel('goal4inner', 0.62, 0.65, 0, 0.065, 0.065, 0.018+overlapMargin, 'cylindergrey.data');
+  makeModel('cylinderside1', -0.45, -0.45, 0, 0.035, 0.035, 0.018, 'cylinder.data');
+  makeModel('cylinderside2', -0.45, -0.45, 0, 0.025, 0.025, 0.018+overlapMargin, 'cylinderlight.data');
+  makeModel('cylinderside3', 0.45, -0.45, 0, 0.035, 0.035, 0.018, 'cylinder.data');
+  makeModel('cylinderside4', 0.45, -0.45, 0, 0.025, 0.025, 0.018+overlapMargin, 'cylinderlight.data');
+  makeModel('cylinderside5', -0.45, 0.45, 0, 0.035, 0.035, 0.018, 'cylinder.data');
+  makeModel('cylinderside6', -0.45, 0.45, 0, 0.025, 0.025, 0.018+overlapMargin, 'cylinderlight.data');
+  makeModel('cylinderside7', 0.45, 0.45, 0, 0.035, 0.035, 0.018, 'cylinder.data');
+  makeModel('cylinderside8', 0.45, 0.45, 0, 0.025, 0.025, 0.018+overlapMargin, 'cylinderlight.data');
+  makeModel('goal1', -0.66, -0.66, 0, 0.055, 0.055, 0.018, 'cylinder.data');
+  makeModel('goal2', 0.66, -0.66, 0, 0.055, 0.055, 0.018, 'cylinder.data');
+  makeModel('goal3', -0.66, 0.66, 0, 0.055, 0.055, 0.018, 'cylinder.data');
+  makeModel('goal4', 0.66, 0.66, 0, 0.055, 0.055, 0.018, 'cylinder.data');
+  makeModel('goal1inner', -0.66, -0.66, 0, 0.05, 0.05, 0.018+overlapMargin, 'cylindergrey.data');
+  makeModel('goal2inner', 0.66, -0.66, 0, 0.05, 0.05, 0.018+overlapMargin, 'cylindergrey.data');
+  makeModel('goal3inner', -0.66, 0.66, 0, 0.05, 0.05, 0.018+overlapMargin, 'cylindergrey.data');
+  makeModel('goal4inner', 0.66, 0.66, 0, 0.05, 0.05, 0.018+overlapMargin, 'cylindergrey.data');
   setInterval(drawScene, 33); //30 fps
 }
 
@@ -158,15 +157,16 @@ function matrixMultiply(mat1, mat2){
 var temp = 0.0;
 
 function drawScene(){
+  screenVisible = 1;
   for(var key in models){
     var model = models[key];
     console.log(model);
     temp += 0.05
-    viewMatrix = makeScale(0.74, 0.74, 0.74);
-    viewMatrix = matrixMultiply(viewMatrix, makeXRotation(90 * (3.14/180)));
-    viewMatrix = matrixMultiply(viewMatrix, makeYRotation(temp * (3.14/180)));
-    viewMatrix = matrixMultiply(viewMatrix, makeXRotation(-40 * (3.14/180)));
-    viewMatrix = matrixMultiply(viewMatrix, makeZToWMatrix(0.5));
+    cameraMatrix = makeScale(0.56, 0.56, 0.56);
+    cameraMatrix = matrixMultiply(cameraMatrix, makeXRotation(90 * (3.14/180)));
+    cameraMatrix = matrixMultiply(cameraMatrix, makeYRotation(temp * (3.14/180)));
+    cameraMatrix = matrixMultiply(cameraMatrix, makeXRotation(-40 * (3.14/180)));
+    MVPMatrix = matrixMultiply(cameraMatrix, makeZToWMatrix(0.9));
     createModel('cubex', model['center'][0], model['center'][1], model['center'][2], model['scale'][0],  model['scale'][1],  model['scale'][2], model['filedata'], model['filename']);
   }
 }
@@ -359,13 +359,15 @@ function createModel(name, x_pos, y_pos, z_pos, x_scale, y_scale, z_scale, filed
 
     var u_matrix = gl.getUniformLocation(program, "u_matrix");
     //matrix = matrixMultiply(matrix, makeYRotation(69 * (3.14/180)));
-    gl.uniformMatrix4fv(u_matrix, false, viewMatrix);
+    gl.uniformMatrix4fv(u_matrix, false, MVPMatrix);
 
     //console.log(vertex_buffer_data);
     console.log(vertex_buffer_data.length);
 
     // draw
-    gl.drawArrays(gl.TRIANGLES, 0, vertex_buffer_data.length/3);
+    if (screenVisible == 1){
+      gl.drawArrays(gl.TRIANGLES, 0, vertex_buffer_data.length/3);
+    }
     var mymodel = {'center':[x_pos,y_pos,z_pos], 'scale':[x_scale,y_scale,z_scale], 'name':name, 'filedata':filedata, 'filename':filename};
     models[name] = mymodel;
 }
